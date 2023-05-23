0bb76509ff832aee01ca0d2cea42419c1f982c03
gocum <cetacue@misena.edu.co>
2023-05-22 23:29:19 -0500

    Realize la funcion de tipo anonima

--- js/Flecha.js
+++ js/Flecha.js
@@ -1,6 +1,13 @@
-let nombre=prompt(`Inserta tu nombre en este espacio`);
-let apellido=prompt(`Inserta tu apellido en este espacio`);
-let flecha = (nomnbre,apellido)=>{
-    console.log(`Esta es una funcion de tipo flecha mi estimado ${nombre} ${apellido}`);
-}
-flecha(nombre,apellido);
+// //funcion tipo flecha
+// let nombre=prompt(`Inserta tu nombre en este espacio`);
+// let apellido=prompt(`Inserta tu apellido en este espacio`);
+// let flecha = (nomnbre,apellido)=>{
+//     console.log(`Esta es una funcion de tipo flecha mi estimado ${nombre} ${apellido}`);
+// }
+// flecha(nombre,apellido);
+//funcion reservada---------------------------------------------------------------------------------
+let nombre= prompt('Cual es tu nombre')
+let Funcion2=function Nombre(){
+console.log(`Otro tipo de funcion(anonima-no hoisting) mi estimado ${nombre}`)
+};
+Funcion2(nombre);
