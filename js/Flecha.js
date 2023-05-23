3701b9e6af96944784cb4f1c6cad1d52ca5bd679
gocum <cetacue@misena.edu.co>
2023-05-22 23:21:54 -0500

    Hola JS

---
+++ js/Flecha.js
@@ -0,0 +1,6 @@
+let nombre=prompt(`Inserta tu nombre en este espacio`);
+let apellido=prompt(`Inserta tu apellido en este espacio`);
+let flecha = (nomnbre,apellido)=>{
+    console.log(`Esta es una funcion de tipo flecha mi estimado ${nombre} ${apellido}`);
+}
+flecha(nombre,apellido);
